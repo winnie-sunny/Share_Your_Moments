79f3265e783dcf6cb5b2a9d83f066f4b79d98385
Wen Jia <winniejia01@gmail.com>
2023-07-26 18:36:34 -0400

    add authentication

---
+++ network/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+
+class AuthenticatedSessionController extends Controller
+{
+    /**
+     * Handle an incoming authentication request.
+     */
+    public function store(LoginRequest $request): JsonResponse
+    {
+        $request->authenticate();
+
+        $request->session()->regenerate();
+
+        //return response()->noContent();
+        $token = $request->user()->createToken('api-token')->plainTextToken;
+        return response()->json(['$token' => $token]);
+    }
+
+    /**
+     * Destroy an authenticated session.
+     */
+    public function destroy(Request $request): Response
+    {
+        $request->user()->tokens()->delete();
+        
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return response()->noContent();
+    }
+}
--- network/routes/api.php
+++ network/routes/api.php
@@ -11,15 +11,19 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('user.posts',PostController::class);
-Route::apiResource('posts', PostController::class);
-Route::apiResource('post.comments', CommentController::class);
+
+//protected group of routes
+Route::middleware(['auth:sanctum'])->group(function(){
+    Route::apiResource('user.posts',PostController::class);
+    Route::apiResource('posts', PostController::class);
+    Route::apiResource('post.comments', CommentController::class);
+});
